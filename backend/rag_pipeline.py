901d94409919f6bd084bb46d36995ba0afe5f4b2
Chinmayi S R <raravi.chinmayi@gmail.com>
2025-06-26 18:18:58 +0530

    spacing changes

--- backend/rag_pipeline.py
+++ backend/rag_pipeline.py
@@ -12,11 +12,13 @@ def run_rag_pipeline(news_text, links):
     docs = loader.load()
     splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     splits = splitter.split_documents(docs)
+
     from langchain_community.vectorstores import Chroma
     from langchain_huggingface import HuggingFaceEmbeddings
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
     vectorstore = Chroma.from_documents(documents=splits, embedding=embeddings)
     retriever = vectorstore.as_retriever(search_kwargs={"k": 2})
+    
     from langchain_ollama import OllamaLLM
     llm = OllamaLLM(model="llama3")
     from langchain.prompts import ChatPromptTemplate
